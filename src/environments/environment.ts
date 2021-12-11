2b2a69e87b236affd6ef7f2d0da5d836bd99270a
Ezequias Gabriel Ramirez <ezequias.gabriel.rs@gmail.com>
2021-12-11 11:01:45 -0500

    Crear componentes y moules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 export const environment = {
   production: false
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyAb0dMM1rKT22JUi2zdTh8jVu9TLjK4qNk",
+  authDomain: "fb-clone2021.firebaseapp.com",
+  projectId: "fb-clone2021",
+  storageBucket: "fb-clone2021.appspot.com",
+  messagingSenderId: "958156690165",
+  appId: "1:958156690165:web:783b50a0af67c657b56bbc"
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
